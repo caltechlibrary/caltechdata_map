c80e8a822d0229ede6ed435ce772138aeccf182b
Tom Morrell <tmorrell@caltech.edu>
2018-02-14 20:17:29 +0000

    Cleanup

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Run make_map.
 
-echo "____________________________" >> out.log
+cd /home/ubuntu/caltechdata_map
+echo "____________________________" > out.log
 echo $(date -u) >> out.log
 python make_map.py >> out.log
 aws s3 cp caltechdata_map.html s3://maps.library.caltech.edu --acl public-read
